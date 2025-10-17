31ab26424105ce1482119f48e3eb9c653fa98ec9
ManojNambiraj <postboxmano@gmail.com>
2025-10-17 20:47:00 +0530

    Node CRUD Final

--- controller/userController.js
+++ controller/userController.js
@@ -23,27 +23,78 @@ const controller = {
 
   async users(req, res) {
     try {
-       const usersData = await User.find()
+      const usersData = await User.find();
 
-       res.status(200).json({ data: usersData });
+      res.status(200).json({ data: usersData });
     } catch (error) {
       res.status(500).json({ message: "Something went wrong" });
     }
   },
 
-  async user(req, res){
-     try {
-       
-        const { id } = req.params;
+  async user(req, res) {
+    try {
+      const { id } = req.params;
 
-        const userData = await User.findOne({_id: id})
+      const userData = await User.findOne({ _id: id });
 
-        res.status(200).json({data: userData})
+      res.status(200).json({ data: userData });
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  },
 
-     } catch (error) {
-       res.status(500).json({ message: "Something went wrong" });
-     }
-  }
+  async updateUser(req, res) {
+    try {
+      const { id } = req.params;
+      const { name, age, email, mobile, password } = req.body;
+
+      const userData = await User.findOne({ _id: id });
+
+      if (userData) {
+        const updatedUser = await User.updateOne(
+          { _id: id },
+          {
+            $set: {
+              name,
+              age,
+              email,
+              mobile,
+              password,
+            },
+          }
+        );
+
+        res
+          .status(200)
+          .json({ message: "User Updated successfully", data: updatedUser });
+      } else {
+        res.status(404).json({ message: "User not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+
+      const {id} = req.params
+
+      const existingUser = await User.findOne({_id: id})
+
+      if(existingUser){
+        const deletedUser = await User.deleteOne({_id: id})
+
+        res.status(200).json({message: "User deleted successfully", data: deletedUser})
+
+      }else{
+        res.status(404).json({message: "User not found"})
+      }     
+
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  },
 };
 
 module.exports = controller;
--- router/userRouter.js
+++ router/userRouter.js
@@ -4,5 +4,7 @@ const userController = require("../controller/userController")
 router.post("/create", userController.createUser)
 router.get("/users", userController.users)
 router.get("/user/:id", userController.user)
+router.put("/edit/:id", userController.updateUser)
+router.delete("/delete/:id", userController.deleteUser)
 
 module.exports = router
